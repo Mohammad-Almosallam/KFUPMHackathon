8f7596f8e2af370183fb1159566a842ac8cccad8
Mohammad Almosallam <moh.almusallam4@gmail.com>
2023-02-18 19:06:41 +0300

    feat: animation

--- HACKATHON/frontend/src/components/HowItWorks.jsx
+++ HACKATHON/frontend/src/components/HowItWorks.jsx
@@ -5,37 +5,67 @@ function HowItWorks() {
   return (
     <div>
       <div class="mx-auto container mt-40 f-f-p px-4 xl:px-0 py-24">
-        <h1 class="focus:outline-none text-center text-4xl pb-16 lg:text-6xl font-bold dark:text-white lg:leading-9 tracking-wider text-gray-900">
+        <h1
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          class="focus:outline-none text-center text-4xl pb-16 lg:text-6xl font-bold dark:text-white lg:leading-9 tracking-wider text-gray-900"
+        >
           How it works?
         </h1>
-        <div class="md:mt-24 f-f-p">
+        <div
+          data-aos="fade-down"
+          data-aos-duration="1000"
+          class="md:mt-24 f-f-p"
+        >
           <div class="hidden md:flex justify-center w-full">
             <div class="flex flex-col items-center md:items-end md:pr-12 md:border-r-4 border-gray-300 relative md:w-1/2">
-              <div aria-label="sign up" role="img">
+              <div
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                aria-label="sign up"
+                role="img"
+              >
                 <img
-                  class="focus:outline-none mt-10 filter grayscale"
+                  class="focus:outline-none mt-10 "
                   src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg1.svg"
                   alt="how it work"
                 />
-                <IoPersonOutline class="focus:outline-none mt-10 filter grayscale" />
               </div>
-              <div aria-label="2" role="img">
+              <div
+                data-aos="fade-right"
+                data-aos-duration="1000"
+                aria-label="2"
+                role="img"
+              >
                 <img
-                  class="focus:outline-none mt-24 filter grayscale"
+                  class="focus:outline-none mt-24 "
                   src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg2.svg"
                   alt=""
                 />
               </div>
               <div class="flex mt-12 flex-col items-center lg:items-end md:w-8/12">
-                <h1 class="focus:outline-none dark:text-white  text-xl font-bold leading-5">
+                <h1
+                  data-aos="fade-right"
+                  data-aos-duration="1000"
+                  class="focus:outline-none dark:text-white  text-xl font-bold leading-5"
+                >
                   Search
                 </h1>
-                <h2 class="focus:outline-none text-gray-500 dark:text-gray-200 mt-3 pl-3 text-center md:text-right text-base leading-6 tracking-wide">
+                <h2
+                  data-aos="fade-right"
+                  data-aos-duration="1500"
+                  class="focus:outline-none text-gray-500 dark:text-gray-200 mt-3 pl-3 text-center md:text-right text-base leading-6 tracking-wide"
+                >
                   Search for the best matching roommate according to our
                   customizable preferences.
                 </h2>
               </div>
-              <div aria-label="transactions" role="img">
+              <div
+                data-aos="fade-right"
+                data-aos-duration="1500"
+                aria-label="transactions"
+                role="img"
+              >
                 <img
                   class="focus:outline-none mt-24"
                   src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg3.svg"
@@ -50,31 +80,54 @@ function HowItWorks() {
             </div>
 
             <div class="flex flex-col items-center md:items-start md:pl-12 lg:border-gray-400 mt-20 md:mt-0 md:w-1/2">
-              <div aria-label="1" role="img">
+              <div
+                data-aos="fade-left"
+                data-aos-duration="1000"
+                aria-label="1"
+                role="img"
+              >
                 <img
-                  className="filter grayscale"
+                  className=""
                   src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg4.svg"
                   alt=""
                 />
               </div>
 
               <div class="flex mt-6 flex-col items-center md:items-start md:w-8/12">
-                <h1 class="focus:outline-none dark:text-white  text-xl font-bold leading-5">
+                <h1
+                  data-aos="fade-left"
+                  data-aos-duration="1000"
+                  class="focus:outline-none dark:text-white  text-xl font-bold leading-5"
+                >
                   Sign up
                 </h1>
-                <h2 class="focus:outline-none text-gray-500 dark:text-gray-200 mt-3 text-base leading-6 tracking-wide">
+                <h2
+                  data-aos="fade-left"
+                  data-aos-duration="1500"
+                  class="focus:outline-none text-gray-500 dark:text-gray-200 mt-3 text-base leading-6 tracking-wide"
+                >
                   Sign up on our website and add your preferences that suits you
                   the best.
                 </h2>
               </div>
-              <div aria-label="wallet" role="img">
+              <div
+                data-aos="fade-left"
+                data-aos-duration="1000"
+                aria-label="wallet"
+                role="img"
+              >
                 <img
-                  class="focus:outline-none mt-32 filter grayscale"
+                  class="focus:outline-none mt-32 "
                   src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg5.svg"
                   alt=""
                 />
               </div>
-              <div aria-label="3" role="img">
+              <div
+                data-aos="fade-left"
+                data-aos-duration="1000"
+                aria-label="3"
+                role="img"
+              >
                 <img
                   class="focus:outline-none mt-20"
                   src="https://tuk-cdn.s3.amazonaws.com/can-uploader/3_step_center_aligned_how_it_worksSvg6.svg"
@@ -82,10 +135,18 @@ function HowItWorks() {
                 />
               </div>
               <div class="flex mt-6 flex-col items-center md:items-start md:w-8/12">
-                <h1 class="focus:outline-none text-xl dark:text-white font-bold leading-5">
+                <h1
+                  data-aos="fade-left"
+                  data-aos-duration="1000"
+                  class="focus:outline-none text-xl dark:text-white font-bold leading-5"
+                >
                   Connect
                 </h1>
-                <h2 class="focus:outline-none text-gray-500 dark:text-gray-200 mt-3 text-base leading-6 tracking-wide">
+                <h2
+                  data-aos="fade-left"
+                  data-aos-duration="1500"
+                  class="focus:outline-none text-gray-500 dark:text-gray-200 mt-3 text-base leading-6 tracking-wide"
+                >
                   Connect to your matching mate via his contact information.
                 </h2>
               </div>
